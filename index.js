500966e9eaa6d9aeeb1eb80522ce6838cbe8e4bb
Karolis Bliodnieks <karolis@scale3c.com>
2021-03-20 20:40:46 +0200

    add: json input, add: canvas output

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
+const out = fs.createWriteStream(__dirname + "/test.png");
+const { createCanvas, loadImage } = require("canvas");
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +15,25 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res, next) => {
-  res.render("index.ejs");
+  let htmlFile = fs.readFileSync(path.join(__dirname, "label.html"));
+  res.render("index.ejs", { htmlFile });
+});
+
+app.post("/create", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+
+  const width = 500;
+  const height = 500;
+
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, width, height);
+  const stream = canvas.createPNGStream();
+  stream.pipe(out);
+  out.on("finish", () => console.log("The PNG file was created."));
 });
 
 app.listen(PORT, function () {
