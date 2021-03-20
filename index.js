ffa58af57a707319d5cbf269789195fe42c12efa
Karolis Bliodnieks <karolis@scale3c.com>
2021-03-20 14:14:17 +0200

    add: canvas height and width options

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
 const PORT = 3000;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send('hi')
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res, next) => {
+  res.render("index.ejs");
+});
 
 app.listen(PORT, function () {
   console.log(`Server is starting on port ${PORT}`);
-});``
+});
