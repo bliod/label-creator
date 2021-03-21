1ac906201883919e44d38403a43e8212bbe52f06
Karolis Bliodnieks <karolis@scale3c.com>
2021-03-21 20:05:06 +0200

    add: submit, cleanup

--- controlers/createLabel.js
+++ controlers/createLabel.js
@@ -3,69 +3,81 @@ const fs = require("fs");
 const { createCanvas, loadImage, Image } = require("canvas");
 
 const createLabel = async (req, res, next) => {
-  const out = fs.createWriteStream(path.join(__dirname, "../public/label.png"));
-  const data = req.body;
+  try {
+    const out = fs.createWriteStream(
+      path.join(__dirname, "../public/label.png")
+    );
+    const data = req.body;
+    let width = 400;
+    let height = 600;
+    data.canvas?.height ? (height = Number(data.canvas.height)) : "";
+    data.canvas?.width ? (width = Number(data.canvas.width)) : "";
 
-  let width = 400;
-  let height = 600;
-  data.canvas?.height ? (height = Number(data.canvas.height)) : "";
-  data.canvas?.width ? (width = Number(data.canvas.width)) : "";
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    context.lineWidth = 4;
+    context.strokeStyle = "black";
+    context.strokeRect(0, 0, width, height);
+    context.fillStyle = "#fff";
 
-  const canvas = createCanvas(width, height);
-  const context = canvas.getContext("2d");
-  context.lineWidth = 4;
-  context.strokeStyle = "black";
-  context.strokeRect(0, 0, width, height);
-  context.fillStyle = "#fff";
-
-  if (data.textElements) {
-    if (data.textElements[0]) {
-      data.textElements.forEach((element) => {
-        context.fillStyle = element.color;
-        context.font = element.font;
-        context.fillText(
-          element.text,
-          element.position?.x,
-          element.position?.y,
-          element.width
-        );
-      });
+    if (data.textElements) {
+      if (data.textElements[0]) {
+        data.textElements.forEach((element) => {
+          context.fillStyle = element.color;
+          context.font = element.font;
+          context.fillText(
+            element.text,
+            element.position?.x,
+            element.position?.y,
+            element.width
+          );
+        });
+      }
     }
-  }
-  if (data.rectangleElements) {
-    if (data.rectangleElements[0]) {
-      data.rectangleElements.forEach((element) => {
-        context.lineWidth = element.stroke?.size;
-        context.strokeStyle = element.stroke?.color;
-        context.strokeRect(
-          element.position?.x,
-          element.position?.y,
-          element.width,
-          element.height
-        );
-      });
-    }
-  }
-  if (data.imageElements) {
-    if (data.imageElements[0]) {
-      data.imageElements.forEach((element) => {
-        loadImage(element.url).then((image) => {
-          context.drawImage(
-            image,
+    if (data.rectangleElements) {
+      if (data.rectangleElements[0]) {
+        data.rectangleElements.forEach((element) => {
+          context.lineWidth = element.stroke?.size;
+          context.strokeStyle = element.stroke?.color;
+          context.strokeRect(
             element.position?.x,
             element.position?.y,
             element.width,
             element.height
           );
-          const buffer = canvas.toBuffer("image/png");
-          fs.writeFileSync(path.join(__dirname, "../public/label.png"), buffer);
         });
-      });
+      }
     }
+    if (data.imageElements) {
+      if (data.imageElements[0]) {
+        data.imageElements.forEach((element) => {
+          loadImage(element.url).then((image) => {
+            context.drawImage(
+              image,
+              element.position?.x,
+              element.position?.y,
+              element.width,
+              element.height
+            );
+            const buffer = canvas.toBuffer("image/png");
+            fs.writeFileSync(
+              path.join(__dirname, "../public/label.png"),
+              buffer
+            );
+          });
+        });
+      }
+    }
+    const stream = canvas.createPNGStream();
+    stream.pipe(out);
+    out.on("finish", () => {
+      console.log("The PNG file was created.");
+    });
+    out.on("error", (err) => console.log(err));
+    res.status(201).end();
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
-  const stream = canvas.createPNGStream();
-  stream.pipe(out);
-  out.on("finish", () => console.log("The PNG file was created."));
-  out.on("error", (err) => console.log(err));
 };
 module.exports = createLabel;
--- controlers/getLabel.js
+++ controlers/getLabel.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const getLabel = (req, res, next) => {
   let htmlFile = fs.readFileSync(path.join(__dirname, "../label.html"));
   let example = fs.readFileSync(path.join(__dirname, "../example.json"));
+
   res.render("index.ejs", { htmlFile, example });
 };
 
